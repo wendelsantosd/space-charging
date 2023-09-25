8b0073c54ce24e283bea809dcdf289f70b1004ea
wendelsantosd <wendelwcsantos@gmail.com>
2023-09-24 23:42:57 -0300

    feat: implements query to install station

--- src/modules/station/api/resolvers/station.resolver.ts
+++ src/modules/station/api/resolvers/station.resolver.ts
@@ -1,16 +1,27 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { makeStation } from '@modules/station/repository';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { makeExoplanets } from '@shared/providers';
-import { Exoplanet } from '../dtos';
+import { CreateStationInput, Exoplanet } from '../dtos';
 
 @Resolver()
 export class StationResolver {
   @Query(() => [Exoplanet])
-  public async suitablePlanets() {
+  async suitablePlanets() {
     try {
       const response = await makeExoplanets().getIdealExoplanets();
       return response.data;
     } catch (error) {
       throw new Error('Ocorreu um erro ao listar os exoplanetas');
+    }
+  }
+
+  @Mutation(() => String)
+  async installStation(@Args('data') data: CreateStationInput) {
+    try {
+      await makeStation().createStation(data);
+      return 'Estação instalada com sucesso';
+    } catch (error) {
+      throw new Error('Ocorreu um erro ao criar estação');
     }
   }
 }
